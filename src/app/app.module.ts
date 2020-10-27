0bdc047c5e5b3e40955a6236907e950fd8c3536d
dmarmijosa <daniel2667@hotmail.es>
2020-10-26 20:07:11 -0500

    Esta es una primera version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+//Rutas
+import{APP_ROUTING} from './app.routes';
 
+
+//Servicios
+
+
+//Componentes
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+//CREAR COMPONENTES SIN CSS Y SIN SPEC 
+//ng g c components/home  --inline-style --skipTests=true
+
 export class AppModule { }
